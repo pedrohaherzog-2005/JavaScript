aa16736cdf39eac3bd51cdf736e92a8c53e313da
Pedro Henrique A. Herzog <pedrohaherzog2005@gmail.com>
2024-07-08 10:23:57 -0300

    Função While e Do While(Enquanto e Faça Enquanto).

--- JsAula13/ex006.js
+++ JsAula13/ex006.js
@@ -1,15 +1,31 @@
-var c = 1
+// Função Padrão(console.log):
+
+/* console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+console.log('Passo 4')
+console.log('Passo 5')
+console.log('Passo 6')
+console.log('Passo 7')
+console.log('Passo 8')
+console.log('Passo 9')
+console.log('Passo 10')
+console.log('Passo 11')
+console.log('Passo 12')
+console.log('Passo 13') */
+
+// Função enquanto(while):
+
+/* var c = 1
 while (c <= 13) {
     console.log(`Passo ${c}`)
     c++
-}
+} */
 
-/* console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?') */
+// Funcão faça enquanto(do while):
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 13)

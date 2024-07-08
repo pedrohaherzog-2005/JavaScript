e3cfbaf0d12a6a96f4e933e8a405a6097e72bb83
Pedro Henrique A. Herzog <pedrohaherzog2005@gmail.com>
2024-07-08 10:00:21 -0300

    Novo ex006.js

---
+++ JsAula13/ex006.js
@@ -0,0 +1,14 @@
+var c = 1
+while (c <= 9) {
+    console.log(`Passo ${c}`)
+}
+
+/* console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') */

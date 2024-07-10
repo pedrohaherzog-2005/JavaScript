ebf3ce8a1058e8d461333f198e4f80f605a72029
Pedro Henrique A. Herzog <pedrohaherzog2005@gmail.com>
2024-07-10 11:08:31 -0300

    Update ex007.js

--- JsAula14/ex007.js
+++ JsAula14/ex007.js
@@ -1,1 +1,22 @@
-/* var ex007 = 'ex007.js' */
+/* var c = 1
+// Função enquanto(while):
+
+while (c <= 13) {
+    console.log(c)
+    c++
+}
+
+// Funcão faça enquanto(do while):
+
+do {
+    console.log(c)
+    c++
+} while (c <= 13) */
+
+// Função para(for):
+
+console.log('Vai começar...!')
+for(var c = 1;c <= 13;c++) {
+    console.log(c)
+}
+console.log('FIM!')

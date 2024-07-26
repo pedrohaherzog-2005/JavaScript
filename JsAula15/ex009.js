63691d7e3e4711751a4650f88e1a8328b7a8171c
Pedro Henrique A. Herzog <pedrohaherzog2005@gmail.com>
2024-07-26 09:31:50 -0300

    UPDATE: Uso do FOR IN e do num.indexOf().

--- JsAula15/ex009.js
+++ JsAula15/ex009.js
@@ -1,16 +1,24 @@
 let valores = [8, 1, 7, 4, 2, 9]
-
-console.log(valores)
-
+valores.sort()
+//console.log(valores)
+/* -------------------------- */
 // Primeira Função
 /* console.log(valores[0])
 console.log(valores[1])
 console.log(valores[2])
 console.log(valores[3])
 console.log(valores[4])
-console.log(valores[5]) */
+console.log(valores[5])
+/* -------------------------- */
 
 // Segunda Função
-for (var pos=0; pos < valores.length; pos++) {
+/* for (var pos=0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+} */
+
+/* --------------------------------------------------------- */
+
+// Terceira função simplificada
+for (var pos in valores) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }

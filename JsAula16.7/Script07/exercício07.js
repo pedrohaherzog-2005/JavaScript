cd21ed41e6332020e91024eb0c383b553e97d4da
Pedro Henrique A. Herzog <pedrohaherzog2005@gmail.com>
2024-07-26 11:36:32 -0300

    NEW: Nova pasta JsAula16.7

---
+++ JsAula16.7/Script07/exercício07.js
@@ -0,0 +1,8 @@
+var num = document.querySelector('input#fnum')
+var lista = document.querySelector('select#flista')
+var res = document.querySelector('div#res')
+let valores = []
+
+function adicionar() {
+    //
+}

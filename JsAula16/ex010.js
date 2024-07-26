083b714dfc649f26396e8daacabd5dcbfd539d85
Pedro Henrique A. Herzog <pedrohaherzog2005@gmail.com>
2024-07-26 10:52:36 -0300

    NEW: Nova pasta de exercícios em javascript.

---
+++ JsAula16/ex010.js
@@ -0,0 +1,9 @@
+/* Função com chamada de parâmetro */
+function parimpar(n) {
+    if (n % 2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+console.log(parimpar(4))
